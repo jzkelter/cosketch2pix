f17cf49c0e34d4539ac5b86401ed9b840b519ca7
Gabriel Caniglia <gcaniglia1@gmail.com>
2019-12-04 22:03:27 -0600

    cleaned up repo

--- collabpix2pix.py
+++ collabpix2pix.py
@@ -1,26 +1,33 @@
-import msa.utils
+"""
+@author: gcanco
+
+Takes line sketch (input.png from input folder) and outputs painterly rendering (output.png into output folder)
+"""
+
 from msa.predictor import Predictor
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import cv2
-import tensorflow as tf
 import numpy as np
-import params
 
+#read input
 img_in = cv2.imread("input/input.png", 1)
 img_out = np.empty([])
 
+#ensure input is 256x256px
 width = 256
 height = 256
 dim = (width, height)
 img_in = cv2.resize(img_in, dim, interpolation = cv2.INTER_AREA)
 
+#run model
 predictor = Predictor(json_path = './models/gart_canny_256.json')
 img_predicted = predictor.predict(img_in)[0]
 img_out = img_predicted
 
+#output correct color mapping
 cv2.imwrite("output/output.png", cv2.cvtColor(img_out.astype('float32')*255, cv2.COLOR_RGB2BGR))
 
 predictor = None
