281d500fb3c1fe7d8322362a80baf119e828b2f4
Gabriel Caniglia <gcaniglia1@gmail.com>
2019-12-09 13:39:52 -0600

    convert input to black and white

--- collabpix2pix.py
+++ collabpix2pix.py
@@ -17,15 +17,16 @@ def run_pix2pix():
     img_in = cv2.imread("input/input.png", 1)
     img_out = np.empty([])
 
-    #ensure input is 256x256px
+    #convert input to black and white 256x256px
     width = 256
     height = 256
     dim = (width, height)
-    img_in = cv2.resize(img_in, dim, interpolation = cv2.INTER_AREA)
+    (thresh, bwimg_in) = cv2.threshold(img_in, 127, 255, cv2.THRESH_BINARY)
+    finimg_in = cv2.resize(bwimg_in, dim, interpolation = cv2.INTER_AREA)
 
     #run model
     predictor = Predictor(json_path = './models/gart_canny_256.json')
-    img_predicted = predictor.predict(img_in)[0]
+    img_predicted = predictor.predict(finimg_in)[0]
     img_out = img_predicted
 
     #output correct color mapping
